3a363456ddb2294cebca5197c1a7d7336f43f4f6
Sakshi Kumari2 <sakshi_kumari2@epam.com>
2026-02-13 19:46:34 +0530

    dashboard app

---
+++ src/DashboardInfo.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function DashboardInfo() {
+  return (
+    <div style={{
+      padding: 20,
+      fontFamily: "Arial",
+      textAlign: "center"
+    }}>
+      <h2>👋 Custom Dashboard App</h2>
+
+      <p><strong>Name:</strong> Sakshi</p>
+      <p><strong>Role:</strong> Developer</p>
+    </div>
+  );
+}
